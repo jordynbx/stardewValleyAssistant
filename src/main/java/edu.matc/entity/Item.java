7102538d1e33cbaca2cb326a728d85466ea6066f
Jordyn Barbiaux <jfisher9@madisoncollege.edu>
2021-02-19 13:02:43 -0600

    Adding hibernate depencies and Item class

---
+++ src/main/java/edu.matc/entity/Item.java
@@ -0,0 +1,105 @@
+package edu.matc.entity;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+
+/**
+ * A class to represent an item
+ *
+ * @author jordynbx
+ */
+@Entity(name = "Item")
+@Table(name = "item")
+public class Item {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
+    @GenericGenerator(name = "native", strategy = "native")
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "type")
+    private String type;
+
+    /**
+     * Instantiates a new item
+     */
+    public Item() {
+    }
+
+    /**
+     * Instantiates a new item
+     *
+     * @param name the item's name
+     * @param type the item's type
+     */
+    public Item(String name, String type) {
+        this.name = name;
+        this.type = type;
+    }
+
+    /**
+     * Gets id.
+     *
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * Sets id.
+     *
+     * @param id the id
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * Gets name.
+     *
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Sets name.
+     *
+     * @param name the name
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * Gets type.
+     *
+     * @return the type
+     */
+    public String getType() {
+        return type;
+    }
+
+    /**
+     * Sets type.
+     *
+     * @param type the type
+     */
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    @Override
+    public String toString() {
+        return "Item{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", type='" + type + '\'' +
+                '}';
+    }
+}
