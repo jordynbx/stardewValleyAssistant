b8b52cd6dea7757b2471c11abf19493ff7e08f66
Jordyn Barbiaux <jfisher9@madisoncollege.edu>
2021-02-26 10:56:24 -0600

    Adding Favorite class

--- src/main/java/edu.matc/entity/Favorite.java
+++ src/main/java/edu.matc/entity/Favorite.java
@@ -1,4 +1,36 @@
 package edu.matc.entity;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+
+/**
+ * The type Favorite.
+ */
+@Entity(name = "Favorite")
+@Table(name = "user_favorites")
+@NoArgsConstructor
+@RequiredArgsConstructor
+@Data
 public class Favorite {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
+    @GenericGenerator(name = "native", strategy = "native")
+    private int id;
+
+    @Column(name = "user_id")
+    @NonNull
+    private int userId;
+
+    @Column(name = "item_id")
+    @NonNull
+    private int itemId;
+
+    @ManyToOne
+    private User user;
 }
