a92af94e230c5f8e988060eb113ab0ad9e51151a
Jordyn Barbiaux <jfisher9@madisoncollege.edu>
2021-04-02 11:43:04 -0500

    Adding user and item delete unit tests for UserSearch

--- src/test/java/edu/matc/persistence/UserSearchDaoTest.java
+++ src/test/java/edu/matc/persistence/UserSearchDaoTest.java
@@ -1,6 +1,8 @@
 package edu.matc.persistence;
 
+import edu.matc.entity.Item;
 import edu.matc.entity.Note;
+import edu.matc.entity.User;
 import edu.matc.entity.UserSearch;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -8,10 +10,14 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 public class UserSearchDaoTest {
 
     GenericDao<UserSearch> dao;
+    GenericDao<User> userDao;
+    GenericDao<Item> itemDao;
+
 
     /**
      * Run set up tasks before each test:
@@ -25,6 +31,9 @@ public class UserSearchDaoTest {
         database.runSQL("cleandb.sql");
 
         dao = new GenericDao<>(UserSearch.class);
+        userDao = new GenericDao<>(User.class);
+        itemDao = new GenericDao<>(Item.class);
+
     }
 
     /**
@@ -35,5 +44,30 @@ public class UserSearchDaoTest {
         List<UserSearch> searches = dao.getByPropertyEqualInt("user", 2);
         assertEquals(7, searches.size());
         assertEquals(2, searches.get(0).getUser().getId());
+    }
+
+    /**
+     * Verifies that if a user is deleted, all notes by that
+     * user is deleted
+     */
+    @Test
+    void deleteSearchesOnUserDeleteSuccess() {
+        userDao.delete(userDao.getById(2));
+
+        List<UserSearch> searches = dao.getByPropertyEqualInt("user", 2);
+        assertEquals(0, searches.size());
+
+    }
+
+    /**
+     * Verifies that if an item is deleted, all searches for that
+     * item are deleted
+     */
+    @Test
+    void deleteSearchesOnItemDeleteSuccess() {
+        itemDao.delete(itemDao.getById(6));
+
+        List<UserSearch> searches = dao.getByPropertyEqualInt("item", 6);
+        assertEquals(0, searches.size());
     }
 }
